99f8138f26782d3b8742ba8daa2a8bc111c04e7f
Alex Zasorin <zasorin.alek@gmail.com>
2025-12-02 22:17:19 -0800

    refactor(day-1): extract line reading into separate function
    Following read_lines example in Rust By Example.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,28 +1,37 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(file_path)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 fn main() {
     let mut dial: u32 = 50;
     let mut count: u32 = 0;
 
-    let file = File::open("./input.txt").unwrap();
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines.map_while(Result::ok) {
+            let dir = &line.chars().next().unwrap();
+            let mut num = line[1..].parse::<u32>().unwrap();
 
-    for line in io::BufReader::new(file).lines().map_while(Result::ok) {
-        let dir = &line.chars().next().unwrap();
-        let mut num = line[1..].parse::<u32>().unwrap();
+            if *dir == 'L' {
+                while num > dial {
+                    num -= dial + 1;
+                    dial = 99;
+                }
 
-        if *dir == 'L' {
-            while num > dial {
-                num -= dial + 1;
-                dial = 99;
+                dial -= num;
+            } else {
+                dial = (dial + num) % 100;
             }
 
-            dial -= num;
-        } else {
-            dial = (dial + num) % 100;
+            count += if dial == 0 { 1 } else { 0 };
         }
-
-        count += if dial == 0 { 1 } else { 0 };
     }
 
     println!("Count: {}", count);
