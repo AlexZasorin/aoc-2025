a93c884e1ce97d1c43c3297d0fb5d566bd24dafc
Alex Zasorin <zasorin.alek@gmail.com>
2025-12-02 22:09:24 -0800

    chore(day-1): remove debug logs

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -12,25 +12,14 @@ fn main() {
         let mut num = line[1..].parse::<u32>().unwrap();
 
         if *dir == 'L' {
-            println!("Dial: {}", dial);
-            println!("Left {}", num);
             while num > dial {
-                // If char is L, check if number is > dial
-                //  If number is > dial, loop, setting dial to 100 and subtracting dial from number, until
-                //  number is < dial
-                num -= (dial + 1);
+                num -= dial + 1;
                 dial = 99;
-
-                println!("Dial: {}, Remaining: {}", dial, num);
             }
 
             dial -= num;
-            println!("Dial: {}", dial);
         } else {
-            println!("Dial: {}", dial);
-            println!("Right {}", num);
             dial = (dial + num) % 100;
-            println!("Dial: {}", dial);
         }
 
         count += if dial == 0 { 1 } else { 0 };
